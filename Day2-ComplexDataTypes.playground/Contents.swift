11015cb2aef0c4456de2151f3a6f226eabc5ba1f
Dave Spina <dspina79@gmail.com>
2021-10-10 16:46:56 -0400

    Basics of array

--- Day2-ComplexDataTypes.playground/Contents.swift
+++ Day2-ComplexDataTypes.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// complex data types
+let person1 = "Mary Ellen"
+let person2 = "John Miller"
+let person3 = "Harriet Spellman"
+
+let persons = [person1, person2, person3]
+persons[2] // displays Harriet Spellman
+
+// arrays with type declaration
+let managers: [String] = ["Les Paulson", "Helen Pearson"]
+managers[1] // Helen Pearson
