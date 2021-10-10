c2de37fd7e976ec9fd744a8ee3b50b7d6a848554
Dave Spina <dspina79@gmail.com>
2021-10-10 16:52:30 -0400

    Basics of sets

--- Day2-ComplexDataTypes.playground/Contents.swift
+++ Day2-ComplexDataTypes.playground/Contents.swift
@@ -11,3 +11,10 @@ persons[2] // displays Harriet Spellman
 // arrays with type declaration
 let managers: [String] = ["Les Paulson", "Helen Pearson"]
 managers[1] // Helen Pearson
+
+
+// sets
+// initializing a set from an array
+let colors = Set(["red", "blue", "yellow"])
+// duplicates are ignored
+let colors2 = Set(["red", "blue", "red", "magenta", "yellow", "blue"]) // only red, blue, magenta, yellow
