06dc50c6a744a82fe6068ca14a30d03bbf4c9fbc
Dave Spina <dspina79@gmail.com>
2021-10-09 21:02:14 -0400

    Started variable day

---
+++ Day1-Variables.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Cocoa
+
+// basic string
+var greeting = "Hello, playground"
+// reset variable
+greeting = "Hello, Bob"
+
+// numerics
+var age = 42
+var earthPop = 7_900_000_000
+
+// multiple lines
+var multiLineString = """
+this is a new line \
+and this is the line below it \
+this line is last
+"""
+
+// doubles and booleans
+var almostPi = 3.14159
+var myTruth = true
+
+
+// string interpolation
+var myName = "Dave"
+var salutation = "Hello, \(myName)"
