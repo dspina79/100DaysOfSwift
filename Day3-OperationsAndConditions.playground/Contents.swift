70b284c88ad00383de24fe38ddee4f6aa1dc8e7c
Dave Spina <dspina79@gmail.com>
2021-10-11 09:37:20 -0400

    Arithmetic operations

--- Day3-OperationsAndConditions.playground/Contents.swift
+++ Day3-OperationsAndConditions.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Arithmetic Operations
+let x = 10
+let y = 5
+let z = 11
+
+let sum = x + y // 15
+let diff = x - y // 5
+let product = x * y // 50
+let quotient = x / y // 2
+let modulus = z  % x // 1
