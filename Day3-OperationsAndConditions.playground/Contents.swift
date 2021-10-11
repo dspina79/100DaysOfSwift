f76c44685c94408abfdffbe85bae10b5d52e0a82
Dave Spina <dspina79@gmail.com>
2021-10-11 09:43:22 -0400

    Operator overloading

--- Day3-OperationsAndConditions.playground/Contents.swift
+++ Day3-OperationsAndConditions.playground/Contents.swift
@@ -10,3 +10,9 @@ let diff = x - y // 5
 let product = x * y // 50
 let quotient = x / y // 2
 let modulus = z  % x // 1
+
+// Operation Overloads
+let doubleSum = 43.0 + 29.7 // 72.7
+let stringSum = "Hello " + "John" // Hello John
+let arraySum = [1, 2] + [3, 4, 5] // [1, 2, 3, 4, 5]
+
