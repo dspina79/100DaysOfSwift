ae2e488fa579ed8918733d9b031015e4689e0337
Dave Spina <dspina79@gmail.com>
2021-10-11 09:51:19 -0400

    Compound assignment operators

--- Day3-OperationsAndConditions.playground/Contents.swift
+++ Day3-OperationsAndConditions.playground/Contents.swift
@@ -16,3 +16,16 @@ let doubleSum = 43.0 + 29.7 // 72.7
 let stringSum = "Hello " + "John" // Hello John
 let arraySum = [1, 2] + [3, 4, 5] // [1, 2, 3, 4, 5]
 
+// Compound Assignment
+var a = 10
+var b = 5
+var c = 6
+
+a += b // 15
+a -= b // 10
+a *= b // 50
+a /= b // 10
+a %= c // 4
+
+var stringA = "Bob"
+stringA += " is a cool person" // Bob is a cool person
