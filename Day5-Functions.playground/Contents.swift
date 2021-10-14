92b72629515cda37363165fa506eb2767b92c65e
Dave Spina <dspina79@gmail.com>
2021-10-13 20:33:20 -0400

    Returning values

--- Day5-Functions.playground/Contents.swift
+++ Day5-Functions.playground/Contents.swift
@@ -14,3 +14,11 @@ func add(x: Int, y: Int) {
 }
 
 add(x: 5, y: 7) // prints The sum of 5 and 7 is 12
+
+// Functions Returning Values
+func square(x: Int) -> Int {
+    return x * x
+}
+
+let squareVal = square(x: 7)
+print(squareVal) // prints 49
