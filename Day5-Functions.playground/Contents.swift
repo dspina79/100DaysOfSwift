67818896b7e74f562f5a34d05ef26c1602d69465
Dave Spina <dspina79@gmail.com>
2021-10-14 01:17:52 -0400

    Omitting and default params in Swift

--- Day5-Functions.playground/Contents.swift
+++ Day5-Functions.playground/Contents.swift
@@ -31,3 +31,19 @@ func calcDistance(from x1: Int, to x2: Int) -> Int {
 
 let dist = calcDistance(from: 5, to: 19)
 print("The distance is \(dist)") // prints 14
+
+// Omitting Paramter Labels
+func multiply(_ x: Int, _ y: Int) -> Int {
+    return x * y
+}
+
+let prodResult = multiply(5, 3)
+print(prodResult) // prints 15s
+
+// Default Parameters
+func sayHello(to person: String, greet: String = "Hello") -> String {
+    return "\(greet), \(person)"
+}
+
+print(sayHello(to: "Dean")) // prints "Hello, Dean"
+print(sayHello(to: "Dean", greet: "Allo")) // prints "Allo, Dean"
