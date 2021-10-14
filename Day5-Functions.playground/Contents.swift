cfa3efb24c4d05fad57217a7e7e21b5defa4cb97
Dave Spina <dspina79@gmail.com>
2021-10-13 20:31:39 -0400

    Function declaration and parameters

--- Day5-Functions.playground/Contents.swift
+++ Day5-Functions.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Functions - Basics
+func sayHelloWorld() {
+    print("Hello, world!")
+}
+
+sayHelloWorld() // prints Hello, world!
+
+// Functions with Parameters
+func add(x: Int, y: Int) {
+    let sum = x + y
+    print("The sum of \(x) and \(y) is \(sum)")
+}
+
+add(x: 5, y: 7) // prints The sum of 5 and 7 is 12
