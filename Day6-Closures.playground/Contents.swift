187b8f341fb4f893ede6662944563276c2c8203c
Dave Spina <dspina79@gmail.com>
2021-10-16 00:11:07 -0400

    Closures with parameters and as parameters

--- Day6-Closures.playground/Contents.swift
+++ Day6-Closures.playground/Contents.swift
@@ -25,3 +25,63 @@ let sum = {(x: Int, y: Int) -> Int in
 let mySum = sum(3, 4) // 7
 print(mySum) // prints 7
 
+
+// Closures as parameters
+let dogBarking = {
+    print("The dog is barking...")
+}
+let catMeowing = {
+    print("The kitty is meowing...")
+}
+
+func animalSpeak(action: () -> Void) {
+    print("Shhh... I hear something.")
+    action()
+    print("Ah, the sound of nature!")
+}
+
+
+animalSpeak(action: dogBarking)
+/*
+ prints:
+ Shhh... I hear something.
+ The dog is barking...
+ Ah, the sound of nature!
+ */
+
+animalSpeak(action: catMeowing)
+/*
+ prints:
+ Shhh... I hear something.
+ The kitty is meowing...
+ Ah, the sound of nature!
+ */
+
+
+let addIt = { (x: Int, y: Int) -> Int in
+    return x + y
+}
+
+let multIt = { (x: Int, y: Int) -> Int in
+    return x * y
+}
+
+func performMath(x: Int, y: Int, mathFunc: (Int, Int) -> Int) {
+    print("Running a math problem with \(x) and \(y)")
+    let result = mathFunc(x, y)
+    print("And the result is \(result)")
+}
+
+performMath(x: 6, y: 5, mathFunc: addIt)
+/*
+ prints:
+ Running a math problem with 6 and 5
+ And the result is 11
+ */
+performMath(x: 6, y: 5, mathFunc: multIt)
+/*
+ prints:
+ Running a math problem with 6 and 5
+ And the result is 30
+ */
+
