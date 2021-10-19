599415ec185ff5c0a4faff55f557006b2a2db93c
Dave Spina <dspina79@gmail.com>
2021-10-19 13:06:35 -0400

    Starting development of structs

--- Day8-Structs.playground/Contents.swift
+++ Day8-Structs.playground/Contents.swift
@@ -2,3 +2,17 @@ import UIKit
 
 // Structs
 
+// Basic Struct with Mutable Properties
+struct GamePlay {
+    var player1: String
+    var player2: String
+    var gameOver: Bool = false
+}
+
+var myFirstGame = GamePlay(player1: "Dean", player2: "Lindsey")
+
+print(myFirstGame.player1) // prints "Dean"
+myFirstGame.player2 = "Sean"
+print(myFirstGame.player2) // prints "Sean"
+myFirstGame.gameOver = true
+
