934e71fee6942156d76bc227c432d657c60959e5
Dave Spina <dspina79@gmail.com>
2021-10-21 01:42:38 -0400

    Struct initializers

--- Day9-MoreStructs.playground/Contents.swift
+++ Day9-MoreStructs.playground/Contents.swift
@@ -13,3 +13,23 @@ var tinkerbell = Fairy(name: "Tinkerbell", wingSpan: 2)
 print("Does \(tinkerbell.name) have wings? \(tinkerbell.hasWings)")
 // prints Does Tinkerbell have wings? true
 
+// Using Initializers
+struct Rectangle {
+    var width: Int
+    var height: Int
+    var internalColor: String
+    var area: Int {
+        return width * height
+    }
+    
+    init(height: Int, width: Int) {
+        self.width = width
+        self.height = height
+        internalColor = "Transparent"
+    }
+}
+
+var rect = Rectangle(height: 4, width: 5)
+rect.internalColor = "red"
+print(rect.area) // prints 20
+
