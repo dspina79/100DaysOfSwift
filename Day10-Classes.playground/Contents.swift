35705daedca6d50f35695d23adc0ededb475d770
Dave Spina <dspina79@gmail.com>
2021-10-21 20:55:02 -0400

    Class basics

--- Day10-Classes.playground/Contents.swift
+++ Day10-Classes.playground/Contents.swift
@@ -1,4 +1,19 @@
 import UIKit
 
 // Classes
+// classes are like structs with several differences
+// including no memberwise initializers
 
+class Fish {
+    var name: String
+    var bonyFish: Bool = true
+    var species: String
+    
+    init(name: String, species: String) {
+        self.name = name
+        self.species = species
+    }
+}
+
+let nemo = Fish(name: "Nemo", species: "Clown Fish")
+print(nemo.bonyFish) // prints true
