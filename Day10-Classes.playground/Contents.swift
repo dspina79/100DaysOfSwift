6f34147123fa16444bf79e71eee62b44f13da908
Dave Spina <dspina79@gmail.com>
2021-10-22 21:32:17 -0400

    Overriding methods

--- Day10-Classes.playground/Contents.swift
+++ Day10-Classes.playground/Contents.swift
@@ -29,4 +29,41 @@ class Goldfish : Fish {
 let moby = Goldfish(name: "Moby")
 print(moby.species) // prints Goldfish
 
+// Override Methods
+class Shape {
+    var side1: Int
+    var side2: Int
+    var side3: Int
+    
+    init(side1: Int, side2: Int, side3: Int) {
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+    }
+    
+    func getPerimiter() -> Int {
+        return side1 + side2 + side3
+    }
+}
 
+class Triangle: Shape {
+    
+}
+
+class Square: Shape {
+    var side4: Int
+    init(side: Int) {
+        self.side4 = side
+        super.init(side1: side, side2: side, side3: side)
+    }
+    
+    override func getPerimiter() -> Int {
+        return side4 * 4
+    }
+}
+
+
+let triangle = Triangle(side1: 3, side2: 5, side3: 4)
+let square = Square(side: 7)
+print(triangle.getPerimiter()) // prints 12
+print(square.getPerimiter()) // prints 28
