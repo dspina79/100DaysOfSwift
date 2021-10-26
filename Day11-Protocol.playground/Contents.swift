e8481691c7fd5e14e1d618f5e5b6c7113312e9f0
Dave Spina <dspina79@gmail.com>
2021-10-26 09:35:08 -0400

    Multiple protocol inheritance

--- Day11-Protocol.playground/Contents.swift
+++ Day11-Protocol.playground/Contents.swift
@@ -32,3 +32,56 @@ printNamedItem(item: toadie)
  The item is named Froggy
  The item is named Toadie
  */
+
+// Protocol Inheritance
+// i.e. Multiple Inheritance
+
+protocol Monitorable {
+    func monitor()
+}
+
+protocol FinderKeeper {
+    var searchParam: String { get set}
+    func findAndKeep() -> Bool
+}
+
+protocol Publicator {
+    func publish(title: String) -> String
+}
+
+struct SystemAdminBot: Monitorable, FinderKeeper, Publicator {
+    var searchParam: String = ""
+    var name: String
+    
+    func monitor() {
+        print("Monitoring system...")
+    }
+    
+    func findAndKeep() -> Bool {
+        if searchParam != "" {
+            print("Found \(searchParam)")
+            return true
+        }
+        
+        return false
+    }
+    
+    func publish(title: String) -> String {
+        print("Publishing \(title)")
+        return "\(title) (Published)"
+    }
+}
+
+var sysbot1 = SystemAdminBot(name: "SysBot1")
+sysbot1.monitor()
+sysbot1.searchParam = "bolts"
+print(sysbot1.findAndKeep())
+print(sysbot1.publish(title: "Do Androids Dream of Electric Sheep?"))
+/*
+ prints:
+ Monitoring system...
+ Found bolts
+ true
+ Publishing Do Androids Dream of Electric Sheep?
+ Do Androids Dream of Electric Sheep? (Published)
+ */
