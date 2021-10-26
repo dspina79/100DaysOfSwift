1447c1ab387a159b3c3ab50898ceb33cb5d7cd55
Dave Spina <dspina79@gmail.com>
2021-10-25 23:11:15 -0400

    Added protocol example

--- Day11-Protocol.playground/Contents.swift
+++ Day11-Protocol.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
 // Protocols
+// similar to interfaces
+
+// a simple type template
+protocol NamedItem {
+    var name: String {get set}
+}
+
+struct Frog: NamedItem {
+    var name: String
+    var color: String
+}
+
+struct Toad: NamedItem {
+    var name: String
+}
+
+func printNamedItem(item: NamedItem) {
+    print("The item is named \(item.name)")
+}
+
+var froggy = Frog(name: "Froggy", color: "Green")
+var toadie = Toad(name: "Toadie")
+
+printNamedItem(item: froggy)
+printNamedItem(item: toadie)
+
+/*
+ prints:
+ The item is named Froggy
+ The item is named Toadie
+ */
