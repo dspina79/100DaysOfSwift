14f35987f9e9ec62a15bcb2925275c52ce00e242
Dave Spina <dspina79@gmail.com>
2021-10-28 21:13:00 -0400

    Unwrapping optionals with if let=

--- Day12-Optionals.playground/Contents.swift
+++ Day12-Optionals.playground/Contents.swift
@@ -11,3 +11,11 @@ age = 30
 print(age) // prints 30
 print(middleName) // prints nil
 
+// Unwrapping an optional
+if let mname = middleName {
+    print("The person's middle name is \(mname)")
+} else {
+    print("No middle name was supplied.")
+}
+// prints No middle name was supplied.
+
