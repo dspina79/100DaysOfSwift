0d810a5845b5233217f470173cd711d6223f3c6c
Dave Spina <dspina79@gmail.com>
2021-10-28 21:08:57 -0400

    Started code on optionals

--- Day12-Optionals.playground/Contents.swift
+++ Day12-Optionals.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 // Optionals
+
+// Simple declaration of an optional
+var age: Int? = nil
+var middleName: String?
+
+age = 30
+
+print(age) // prints 30
+print(middleName) // prints nil
+
