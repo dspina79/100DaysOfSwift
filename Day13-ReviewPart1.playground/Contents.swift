dbcf66c1f89e504c54e85f48573878847cb9b579
Dave Spina <dspina79@gmail.com>
2021-11-01 20:52:54 -0400

    Data types

--- Day13-ReviewPart1.playground/Contents.swift
+++ Day13-ReviewPart1.playground/Contents.swift
@@ -6,3 +6,11 @@ let myConstant: Int = 2
 
 myVariable += 1
 //myConstant += 1 //ilegal
+
+// Types of Data
+let myName: String = "John Lennon"
+let myAge: Int = 48
+let myHourlyWage: Double = 25.39
+let singer: Bool = true
+let otherBandMembers: [String] = ["Ringo", "Paul", "George"]
+
