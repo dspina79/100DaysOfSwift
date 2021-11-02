2edf7350d030a17cb8227d97e61e9cc45213a254
Dave Spina <dspina79@gmail.com>
2021-11-01 21:06:45 -0400

    Operators

--- Day13-ReviewPart1.playground/Contents.swift
+++ Day13-ReviewPart1.playground/Contents.swift
@@ -10,7 +10,32 @@ myVariable += 1
 // Types of Data
 let myName: String = "John Lennon"
 let myAge: Int = 48
+let latitude: Float = 32.93901
 let myHourlyWage: Double = 25.39
+// Apple recommends using Double over Float
 let singer: Bool = true
 let otherBandMembers: [String] = ["Ringo", "Paul", "George"]
+
+
+// Operators
+var x = 10
+var y = 5
+
+// basic arithmetic
+let sum = x + y  // 15
+let diff = x - y // 5
+let prod = x * y // 50
+let quot = x / y // 2
+let mod = x % y // 0
+
+x += 20 // 30
+x *= 10 // 300
+
+let stringA = "ABC"
+let stringB = "abc"
+
+stringA == stringB // false
+stringA != stringB // true
+stringA == "ABC" // true
+
 
