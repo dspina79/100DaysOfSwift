0c9cf41d178b809d0968389c6a33642ffe18a7d9
Dave Spina <dspina79@gmail.com>
2021-11-06 20:51:01 -0400

    Adding functions

--- Day14-ReviewPart2.playground/Contents.swift
+++ Day14-ReviewPart2.playground/Contents.swift
@@ -2,3 +2,23 @@ import UIKit
 
 // Swift Review Part 2
 
+// Functions
+
+func triple(x: Int) -> Int {
+    return x * 3
+}
+
+print(triple(x: 10)) // prints 30
+
+func greeting(_ to: String) {
+    print("Hello, \(to)")
+}
+
+greeting("Linda") // prints Hello, Linda
+
+func add(_ x: Int, _ y: Int) -> Int {
+    return x + y
+}
+
+print(add(3, 10)) // prints 13
+print(add(239, 882)) // prints 13
