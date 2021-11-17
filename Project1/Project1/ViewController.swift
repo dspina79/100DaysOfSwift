6e3ef117d00c60a25e420c09b2f7f561b6592e7a
Dave Spina <dspina79@gmail.com>
2021-11-16 22:25:57 -0500

    Initialized project 1

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Dave Spina on 11/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
