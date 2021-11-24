b84736ec41d984670fc2a972eaad55d89c4327f8
Dave Spina <dspina79@gmail.com>
2021-11-23 23:45:45 -0500

    Added comments

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -14,10 +14,14 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // instantiate the file manager, bundle path and all of the items
+        // in the path
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
+        // retrieve all of the items in the source
         let items = try! fm.contentsOfDirectory(atPath: path)
         
+        // loop filtering
         for item in items {
             if item.hasPrefix("nssl") {
                 pictures.append(item)
@@ -26,17 +30,21 @@ class ViewController: UITableViewController {
         print(pictures)
     }
 
+    // identifies the number of rows to display
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
 
+    // handles loading the information into each cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
     }
     
+    // handles selection of the row
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // storyboard and navigationController are properties of the application
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail")  as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
             navigationController?.pushViewController(vc, animated: true)
